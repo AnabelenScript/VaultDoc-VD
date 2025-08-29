741b09f9e8ce309f44cd70ccf0a290e6da1afebc
JosephAntony37900 <joseantoniopintoaguilar@gmail.com>
2025-08-29 12:16:31 -0600

    add:service users

--- src/app/core/services/user/user_service.ts
+++ src/app/core/services/user/user_service.ts
@@ -1,20 +1,44 @@
-// core/services/profile/profile.service.ts
+// core/services/user/user_service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class ProfileService {
-  private apiUrl = 'http://localhost:8081/users/profile'; // Ajusta la URL según tu backend
+  private apiUrl = 'http://localhost:8081/users/';
 
   constructor(private http: HttpClient) {}
 
   getProfile(): Observable<any> {
-    return this.http.get(this.apiUrl);
+    return this.http.get(this.apiUrl + 'profile');
   }
 
   updateProfile(data: any): Observable<any> {
-  return this.http.put(this.apiUrl, data);
-}
+  return this.http.put(this.apiUrl + 'profile', data);
+  }
+
+  getAllUsers(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
+  getUserById(id: string): Observable<any> {
+    return this.http.get(this.apiUrl + id);
+  }
+
+  updateUserById(id: string, data: any): Observable<any> {
+    return this.http.put(this.apiUrl + id, data);
+  }
+
+  deleteUserById(id: string): Observable<any> {
+    return this.http.delete(this.apiUrl + id);
+  }
+
+  postUser(data: any): Observable<any> {
+    return this.http.post(this.apiUrl, data);
+  }
+
+  getUserByDepartment(department: string): Observable<any> {
+    return this.http.get(this.apiUrl + 'department/' + department);
+  }
 
 }
