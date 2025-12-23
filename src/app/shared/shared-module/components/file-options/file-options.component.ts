8dafd6729fbd5ea9de75ff33afd1a8cda4f4abb6
AlleksDev <aimr2910@gmail.com>
2025-12-23 01:59:15 +0000

    Modal de confirmacion en eliminar archivo agregada

--- src/app/shared/shared-module/components/file-options/file-options.component.ts
+++ src/app/shared/shared-module/components/file-options/file-options.component.ts
@@ -1,13 +1,19 @@
-import { Component, HostListener, Input, Output, EventEmitter } from '@angular/core';
-import { FileServices } from '../../../../core/services/files/files_service';
-import { error, log } from 'console';
-import { PermissionService } from '../../../../core/services/permissions/permissions_services';
-import { UserData } from '../../../../core/services/auth/auth_model';
+import {
+  Component,
+  HostListener,
+  Input,
+  Output,
+  EventEmitter,
+} from "@angular/core";
+import { FileServices } from "../../../../core/services/files/files_service";
+import { error, log } from "console";
+import { PermissionService } from "../../../../core/services/permissions/permissions_services";
+import { UserData } from "../../../../core/services/auth/auth_model";
 
 @Component({
-  selector: 'app-file-options',
-  templateUrl: './file-options.component.html',
-  styleUrl: './file-options.component.css'
+  selector: "app-file-options",
+  templateUrl: "./file-options.component.html",
+  styleUrl: "./file-options.component.css",
 })
 export class FileOptionsComponent {
   @Input() idUser: number | null = 0;
@@ -17,37 +23,56 @@ export class FileOptionsComponent {
   showShareModal: boolean = false;
   userToGivePermission: number = 0;
 
+  showDeleteModal = false;
+
   usersWhitViewPermission: UserData[] = [];
   usersWhitoutViewPermission: UserData[] = [];
   usersWhitChangePermission: UserData[] = [];
   usersWhitoutChangePermission: UserData[] = [];
 
-  constructor(private fileService: FileServices, private permissionService: PermissionService){  }
+  constructor(
+    private fileService: FileServices,
+    private permissionService: PermissionService
+  ) {}
 
-  downloadFile(){
-    console.log("User", this.idUser, "ID", this.idFile, "Nombre archivo", this.filename);
-    
+  downloadFile() {
+    console.log(
+      "User",
+      this.idUser,
+      "ID",
+      this.idFile,
+      "Nombre archivo",
+      this.filename
+    );
+
     if (this.idUser && this.idFile && this.filename) {
       this.fileService.downloadFile(this.idFile, this.idUser, this.filename);
       console.log("Descargando...");
-      
     }
   }
 
-deleteFile(idUser: number | null, idFile: number | null) {
+  openDeleteModal() {
+    this.showDeleteModal = true;
+  }
+
+  closeDeleteModal() {
+    this.showDeleteModal = false;
+  }
+
+  deleteFile(idUser: number | null, idFile: number | null) {
     if (idUser && idFile) {
       this.fileService.deleteFile(idFile, idUser).subscribe(
         (response) => {
           console.log("Archivo eliminado:", response);
 
-          const user = JSON.parse(localStorage.getItem('user_data') || '{}');
+          const user = JSON.parse(localStorage.getItem("user_data") || "{}");
           const history = {
             movimiento: "Eliminación de archivo",
             departamento: user.department,
             id_folder: 0,
             id_file: idFile!,
             id_user: idUser!,
-            fecha_registro: new Date().toISOString()
+            fecha_registro: new Date().toISOString(),
           };
         },
         (error) => console.log("Error:", error)
@@ -56,31 +81,28 @@ deleteFile(idUser: number | null, idFile: number | null) {
   }
 
   getIDRol(): number {
-    let string_user: string | null = localStorage.getItem('user_data')
-    if (string_user != null){
-      let user = JSON.parse(string_user)
-      return user.roleId
+    let string_user: string | null = localStorage.getItem("user_data");
+    if (string_user != null) {
+      let user = JSON.parse(string_user);
+      return user.roleId;
     } else {
-      return 1
+      return 1;
     }
   }
-  
 
-  showModal(){
+  showModal() {
     this.showShareModal = !this.showShareModal;
-    this.getChangePermissions()
+    this.getChangePermissions();
   }
 
-  closeModal(){
-    console.log("Cerrando modal")
+  closeModal() {
+    console.log("Cerrando modal");
     this.showShareModal = false;
     this.modalClosed.emit("");
   }
 
-
-
-  getViewPermissions(){
-    if (this.idFile){
+  getViewPermissions() {
+    if (this.idFile) {
       this.permissionService.getUsersViewPermissions(this.idFile).subscribe(
         (response) => {
           console.log("Respuesta del servidor:", response);
@@ -94,8 +116,8 @@ deleteFile(idUser: number | null, idFile: number | null) {
     }
   }
 
-  getChangePermissions(){
-    if (this.idFile){
+  getChangePermissions() {
+    if (this.idFile) {
       this.permissionService.getUsersChangePermissions(this.idFile).subscribe(
         (response) => {
           console.log("Respuesta del servidor:", response);
@@ -103,80 +125,91 @@ deleteFile(idUser: number | null, idFile: number | null) {
           this.usersWhitoutChangePermission = response.users_wp;
         },
         (error) => {
-          console.log("Error al obtener permisos de modificación:", error)
+          console.log("Error al obtener permisos de modificación:", error);
         }
       );
     }
   }
 
-  giveViewPermission(){
-    if (this.idFile && this.idUser){
-      this.permissionService.grantViewPermission(
-        this.idUser, 
-        {id_file: this.idFile, id_user: this.userToGivePermission},
-      ).subscribe(
-        (response) => {
-          console.log("Respuesta del servidor:", response);
-        },
-        (error) => {
-          console.log("Error al otorgar permiso para ver:", error);
-        }
-      )
-    }
-  }
-
-  giveChangePermission(id_user: number){
+  giveViewPermission() {
     if (this.idFile && this.idUser) {
-      this.permissionService.grantChangePermission(
-        this.idUser,
-        {id_file: this.idFile, id_user: id_user}
-      ).subscribe(
-        (response) => {
-          console.log("Respuesta del servidor:", response);
-          this.getChangePermissions();
-        },
-        (error) => {
-          console.log("Error al otorgar permiso para modificar:", error);
-        }
-      );
+      this.permissionService
+        .grantViewPermission(this.idUser, {
+          id_file: this.idFile,
+          id_user: this.userToGivePermission,
+        })
+        .subscribe(
+          (response) => {
+            console.log("Respuesta del servidor:", response);
+          },
+          (error) => {
+            console.log("Error al otorgar permiso para ver:", error);
+          }
+        );
     }
   }
 
-  revokeViewPermission(){
+  giveChangePermission(id_user: number) {
+    if (this.idFile && this.idUser) {
+      this.permissionService
+        .grantChangePermission(this.idUser, {
+          id_file: this.idFile,
+          id_user: id_user,
+        })
+        .subscribe(
+          (response) => {
+            console.log("Respuesta del servidor:", response);
+            this.getChangePermissions();
+          },
+          (error) => {
+            console.log("Error al otorgar permiso para modificar:", error);
+          }
+        );
+    }
+  }
+
+  revokeViewPermission() {
     if (this.idFile) {
-      this.permissionService.revokeViewPermission({id_file: this.idFile, id_user: this.userToGivePermission}).subscribe(
-        (response) => {
-          console.log("Respuesta del servidor:", response);
-        },
-        (error) => {
-          console.log("Error al quitar permiso de visualizar:", error);
-        }
-      );
+      this.permissionService
+        .revokeViewPermission({
+          id_file: this.idFile,
+          id_user: this.userToGivePermission,
+        })
+        .subscribe(
+          (response) => {
+            console.log("Respuesta del servidor:", response);
+          },
+          (error) => {
+            console.log("Error al quitar permiso de visualizar:", error);
+          }
+        );
     }
   }
 
-  revokeChangePermission(id_user: number){
+  revokeChangePermission(id_user: number) {
     if (this.idFile) {
-      this.permissionService.revokeChangePermission({id_file: this.idFile, id_user: id_user}).subscribe(
-        (response) => {
-          console.log("Respuesta del servidor:", response);
-          this.getChangePermissions();
-        },
-        (error) => {
-          console.log("Error al quitar permiso de modificación:", error);
-        }
-      );
+      this.permissionService
+        .revokeChangePermission({ id_file: this.idFile, id_user: id_user })
+        .subscribe(
+          (response) => {
+            console.log("Respuesta del servidor:", response);
+            this.getChangePermissions();
+          },
+          (error) => {
+            console.log("Error al quitar permiso de modificación:", error);
+          }
+        );
     }
   }
 
-  @HostListener('document:click', ['$event'])
-    onClickOutside(event: MouseEvent){
-      if (this.showShareModal){
-        const target = event.target as HTMLElement;
+  @HostListener("document:click", ["$event"])
+  onClickOutside(event: MouseEvent) {
+    if (this.showShareModal) {
+      const target = event.target as HTMLElement;
 
-        if (target.classList.contains("modal")){
-          this.closeModal()
-        }
+      if (target.classList.contains("modal")) {
+        this.closeModal();
       }
     }
+  }
 }
